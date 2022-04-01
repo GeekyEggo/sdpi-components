13f17c4dc8ae23333674fb4d30237fcb5d3edc7d
Richard Herman <geekyeggo@users.noreply.github.com>
2022-04-02 00:51:05 +0100

    Add checkbox

---
+++ src/components/checkbox.ts
@@ -0,0 +1,70 @@
+import { HTMLInputEvent } from 'dom';
+import { css, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { SettingElement } from './shared/setting-element';
+
+@customElement('sdpi-checkbox')
+export class Checkbox extends SettingElement<boolean> {
+    /** @inheritdoc */
+    public static get styles() {
+        return [
+            ...super.styles,
+            css`
+                .checkbox-container {
+                    border: 1px solid rgba(0, 0, 0, 0.2);
+                    border-radius: 3px;
+                    cursor: pointer;
+                    display: flex;
+                    height: 16px;
+                    margin: 3px 0;
+                    user-select: none;
+                    width: 16px;
+                }
+
+                input {
+                    display: none;
+                }
+
+                .checkmark {
+                    flex: 1 1;
+                }
+
+                input + .checkmark {
+                    background: var(--color-secondary-bg);
+                }
+
+                input:disabled + .checkmark {
+                    opacity: 0.5;
+                }
+
+                input:checked + .checkmark {
+                    background: #77f no-repeat center center;
+                    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='10' viewBox='0 0 12 10'%3E%3Cpolygon fill='%23FFF' points='7.2 7.5 7.2 -1.3 8.7 -1.3 8.6 9.1 2.7 8.7 2.7 7.2' transform='rotate(37 5.718 3.896)'/%3E%3C/svg%3E%0A");
+                    border: 1px solid rgba(0, 0, 0, 0.4);
+                }
+            `
+        ];
+    }
+
+    /**
+     * Gets the optional text to be shown next to the check-box.
+     */
+    @property()
+    public text?: string;
+
+    /** @inheritdoc */
+    protected renderContents(): unknown {
+        return html`
+            <label class="checkbox-container">
+                <input
+                    type="checkbox"
+                    .id=${this.inputID}
+                    .checked=${this.value || false}
+                    .disabled=${this.disabled}
+                    @change=${(ev: HTMLInputEvent<HTMLInputElement>) => this.save(ev.target.checked)}
+                />
+                <span class="checkmark"></span>
+            </label>
+        `;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import './stream-deck/connect';
 
 /* Components */
 import './components/button';
+import './components/checkbox';
 import './components/select';
 import './components/textarea';
 import './components/textfield';
