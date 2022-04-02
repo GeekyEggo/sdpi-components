1948ecde564e281da0c59874ccbf79fceb7e2314
Richard Herman <geekyeggo@users.noreply.github.com>
2022-04-02 01:28:02 +0100

    Update styling of checkbox, add optional text next to checkbox

--- src/components/checkbox.ts
+++ src/components/checkbox.ts
@@ -10,15 +10,16 @@ export class Checkbox extends SettingElement<boolean> {
         return [
             ...super.styles,
             css`
+                :host {
+                    --checkbox-size: 16px;
+                }
+
                 .checkbox-container {
-                    border: 1px solid rgba(0, 0, 0, 0.2);
-                    border-radius: 3px;
-                    cursor: pointer;
-                    display: flex;
-                    height: 16px;
-                    margin: 3px 0;
+                    align-items: center;
+                    display: inline-flex;
+                    margin: var(--spacer) 0;
                     user-select: none;
-                    width: 16px;
+                    width: auto;
                 }
 
                 input {
@@ -26,18 +27,28 @@ export class Checkbox extends SettingElement<boolean> {
                 }
 
                 .checkmark {
-                    flex: 1 1;
-                }
-
-                input + .checkmark {
                     background: var(--color-secondary-bg);
+                    border: 1px solid rgba(0, 0, 0, 0.2);
+                    border-radius: 3px;
+                    flex: var(--checkbox-size) var(--checkbox-size);
+                    height: var(--checkbox-size);
+                    width: var(--checkbox-size);
                 }
 
-                input:disabled + .checkmark {
+                .text {
+                    flex: 1 1;
+                    margin: 0 0 0 var(--spacer);
+                }
+
+                input:not(:disabled) ~ span {
+                    cursor: pointer;
+                }
+
+                input:disabled ~ span {
                     opacity: 0.5;
                 }
 
-                input:checked + .checkmark {
+                input:checked ~ .checkmark {
                     background: #77f no-repeat center center;
                     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='10' viewBox='0 0 12 10'%3E%3Cpolygon fill='%23FFF' points='7.2 7.5 7.2 -1.3 8.7 -1.3 8.6 9.1 2.7 8.7 2.7 7.2' transform='rotate(37 5.718 3.896)'/%3E%3C/svg%3E%0A");
                     border: 1px solid rgba(0, 0, 0, 0.4);
@@ -54,6 +65,8 @@ export class Checkbox extends SettingElement<boolean> {
 
     /** @inheritdoc */
     protected renderContents(): unknown {
+        const text = this.text ? html`<span class="text">${this.text}</span>` : undefined;
+
         return html`
             <label class="checkbox-container">
                 <input
@@ -63,7 +76,8 @@ export class Checkbox extends SettingElement<boolean> {
                     .disabled=${this.disabled}
                     @change=${(ev: HTMLInputEvent<HTMLInputElement>) => this.save(ev.target.checked)}
                 />
-                <span class="checkmark"></span>
+                <span class="checkmark" role="checkbox" aria-checked=${this.value || false}></span>
+                ${text}
             </label>
         `;
     }
