16d287b42423ddd3b5d9d55a15ea3941d270cf54
Richard Herman <geekyeggo@users.noreply.github.com>
2022-05-10 22:58:29 +0100

    Fix version in license

--- example/pi/sdpi-components.js
+++ example/pi/sdpi-components.js
@@ -1,6 +1,6 @@
 /**!
  * @license
- * sdpi-components v1.4.0, Copyright GeekyEggo and other contributors (https://sdpi-components.dev)
+ * sdpi-components v2.0.1, Copyright GeekyEggo and other contributors (https://sdpi-components.dev)
  * Lit, Copyright 2019 Google LLC, SPDX-License-Identifier: BSD-3-Clause (https://lit.dev/)
  */
 (function () {
