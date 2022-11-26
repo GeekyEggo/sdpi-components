e765e94ae4cb012741ac766a978f37b276407f75
Richard Herman <1429781+GeekyEggo@users.noreply.github.com>
2022-11-26 23:04:22 +0000

    Fix font size and styling on Windows

--- example/pi/sdpi-components.js
+++ example/pi/sdpi-components.js
@@ -746,15 +746,15 @@
 
         /* Colors */
         --window-bg-color: #2d2d2d;
-        --font-color: #9a9a9a;
+        --font-color: #969696;
         --input-bg-color: #3d3d3d;
         --input-font-color: #d8d8d8;
 
         --scrollbar-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
 
         /* Typography */
-        --font-family: Arial, 'Segoe UI', Roboto, Helvetica sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-        --font-size: 8pt;
+        --font-family: 'Segoe UI', Arial, Roboto, Helvetica sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+        --font-size: 9pt;
     }
 `;
 
